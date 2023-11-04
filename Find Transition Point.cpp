071ba8bce95c02c9d0d537c00d820e2cabf0b8e3
Gaurav kumar <102864247+yashswag22@users.noreply.github.com>
2023-11-04 18:07:35 +0530

    Create Find Transition Point.cpp

---
+++ Find Transition Point.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:    
+    
+    int transitionPoint(int arr[], int n) {
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==1)
+            return i;
+        }
+        
+        return -1;
+    }
+};
