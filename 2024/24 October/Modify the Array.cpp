b0a6473816634a3a50a4d1937d89123e5f48d699
Gaurav kumar <102864247+yashswag22@users.noreply.github.com>
2024-10-24 12:50:37 +0530

    Update Modify the Array.cpp

--- 2024/24 October/Modify the Array.cpp
+++ 2024/24 October/Modify the Array.cpp
@@ -4,7 +4,7 @@ class Solution {
         
         vector<int> ans;
         int count =0;
-        
+        int n = arr.size();
         for(int i=0;i<arr.size()-1;i++){
             if(arr[i]>0 && arr[i] == arr[i+1])
             {
@@ -14,13 +14,27 @@ class Solution {
             }
             else if(arr[i] == 0) count++;
         }
+        // ========= Method one =============
         
-        for(int i=0;i<arr.size();i++)
-        if(arr[i] != 0) ans.push_back(arr[i]);
+        // for(int i=0;i<arr.size();i++)
+        // if(arr[i] != 0) ans.push_back(arr[i]);
         
-        for(int i=0;i< count;i++)
-        ans.push_back(0);
+        // for(int i=0;i< count;i++)
+        // ans.push_back(0);
         
-        return ans;
+        // return ans;
+        // ========== method two ===========
+        int i=0,j=1;
+        while(i < n && j <n ){
+            if(arr[i] != 0)i++ ;
+            if(arr[j] == 0 || i>j) j ++;
+            
+            if(arr[i] == 0 && arr[j]!=0 && i<j){
+                swap(arr[i],arr[j]);
+                i++;
+                j++;
+            }
+        }
+        return arr;
     }
 };
