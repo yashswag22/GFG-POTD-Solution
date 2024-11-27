790455f0110feff1595e9e533fc90d4494f5253c
Gaurav kumar <102864247+yashswag22@users.noreply.github.com>
2024-11-27 23:44:30 +0530

    Update 27. Smallest Positive Missing Number.cpp

--- 2024/November/27. Smallest Positive Missing Number.cpp
+++ 2024/November/27. Smallest Positive Missing Number.cpp
@@ -3,15 +3,28 @@ class Solution {
     // Function to find the smallest positive number missing from the array.
     int missingNumber(vector<int> &arr) {
         
-        set<int>s(arr.begin(),arr.end());
+        // set<int>s(arr.begin(),arr.end());
         
-        int m = *max_element(arr.begin(),arr.end());
+        // int m = *max_element(arr.begin(),arr.end());
         
-        if(m<=0) return 1;
-        for(int i =1;i<m;i++){
-            if(s.find(i)==s.end()) return i;
+        // if(m<=0) return 1;
+        // for(int i =1;i<m;i++){
+        //     if(s.find(i)==s.end()) return i;
+        // }
+        
+        // return (m+1);
+        
+        // -------- O(n) time complexity ----------
+        int ans =1;
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<=0)continue;
+            
+            else if(arr[i] == ans) ans++;
+            else if(arr[i]< ans) continue;
+            else if(arr[i]>ans) return ans;
         }
-        
-        return (m+1);
+        if(arr[arr.size()-1] <= 0) return 1;
+        return arr[arr.size()-1]+1;
     }
 };
